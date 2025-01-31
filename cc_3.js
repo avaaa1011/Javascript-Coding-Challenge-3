c5805d577d33a302349ef105b34b6502c44d7dde
Ava Tran <ava31@usf.edu>
2025-01-30 23:04:06 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,12 @@ prices.push(26); //adding a new price to the array
 prices.shift(); //removing the first price from the array
 console.log("Updated Prices: ", prices); //printing updated prices after adding and removing
 
+//Task 2 - Modifying Customer Orders
+let orders = [120, 140, 160, 180, 200]; //declaring an array orders with five values 
+console.log("Customer Orders: ", orders);
+orders[2] = 160 +5; //adding 5 to the third order
+console.log("Updated Customer Orders: ", orders);
+let totalOrders = orders.reduce((sum, amount) => sum+amount, 0); //calculating the sum of all orders
+console.log("Total Customer Orders: ", totalOrders);
+
+
