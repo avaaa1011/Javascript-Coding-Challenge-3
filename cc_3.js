1f317a98ccba83695cc912d2b46b5afd2abac51c
Ava Tran <ava31@usf.edu>
2025-01-30 22:56:55 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,7 @@
+//Task 1 - Product Price Management 
+let prices = [21, 22, 23, 24, 25]; //declaring an array prices with five number values
+console.log("Current Prices: ", prices);
+prices.push(26); //adding a new price to the array
+prices.shift(); //removing the first price from the array
+console.log("Updated Prices: ", prices); //printing updated prices after adding and removing
+
