832a34bcf20e112f27bc7ab8687d892edb3bb52c
Ava Tran <ava31@usf.edu>
2025-01-31 18:03:40 -0500

    Made some corrections to the code

--- cc_3.js
+++ cc_3.js
@@ -1,29 +1,24 @@
 //Task 1 - Product Price Management 
 let prices = [21, 22, 23, 24, 25]; //declaring an array prices with five number values
-console.log("Current Prices: ", prices);
 prices.push(26); //adding a new price to the array
 prices.shift(); //removing the first price from the array
 console.log("Updated Prices: ", prices); //printing updated prices after adding and removing
 
 //Task 2 - Modifying Customer Orders
 let orders = [120, 140, 160, 180, 200]; //declaring an array orders with five values 
-console.log("Customer Orders: ", orders);
 orders[2] = 160 +5; //adding 5 to the third order
-console.log("Updated Customer Orders: ", orders);
 let totalOrders = orders.reduce((sum, amount) => sum+amount, 0); //calculating the sum of all orders
 console.log("Total Customer Orders: ", totalOrders);
 
 //Task 3 - Employee Performance Tracking
-let employee //declaring an object with properties
+let employee = //declaring an object with properties 
     {
         name : "TinkerBell",
-        role = "Engineer", 
-        performanceScore = 62,
-        isActive = true
+        role : "Engineer", 
+        performanceScore : 62,
+        isActive : true
     };
-    console.log("Employee: ", employee);
     employee.performanceScore = 84; //updating performance score
-    console.log("Updated info: ", employee);
     employee.promotionalEligible = true; //adding a new property 
         if(employee.performanceScore > 75) //if-else statement to determine promotion eligibility based on performance score
         {
@@ -58,5 +53,4 @@ let inventory = //declaring an object with properties
 }; 
 console.log("Inventory Info: ", inventory);
 console.log("Total Inventory: ", inventory.calculateTotalValue());
-
 
